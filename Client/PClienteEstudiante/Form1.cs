c16876e32d8c68b2b9db56db36b68f94e1658a44
JackBlaze132 <ederdmc@hotmail.com>
2024-09-26 23:10:06 -0500

    fix server and clien

--- Client/PClienteEstudiante/Form1.cs
+++ Client/PClienteEstudiante/Form1.cs
@@ -6,13 +6,11 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Text.Json;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace PClienteEstudiante
 {
-    
     public partial class Form1 : Form
     {
         public Form1()
@@ -22,68 +20,25 @@ namespace PClienteEstudiante
 
         private void button1_Click(object sender, EventArgs e)
         {
-            var options = new RestClientOptions("http://localhost:8081");
+            var options = new RestClientOptions("http://localhost:8090");
             var client = new RestClient(options);
-            var request = new RestRequest("/students/", Method.Get);
-
-            var response = client.Execute(request);
-            var estudiante = JsonSerializer.Deserialize<Estudiante>(response.Content);
-            // Display the student's details in the text fields
-            txtCodigo.Text = estudiante.codigo.ToString();
-            txtNombre.Text = estudiante.nombre;
-
-        }
-
-        /*
-        private void btnPost_Click(object sender, EventArgs e)
-        {
-            var codigo = txtCodigo.Text;
-
-            var options = new RestClientOptions("http://localhost:8081");
-            var client = new RestClient(options);
-            var request = new RestRequest("/students/" + codigo, Method.Post);
+            var request = new RestRequest("/estudiantes/", Method.Get);
 
             var response = client.Execute(request);
             txtCodigo.Text = response.Content;
 
-
-            MessageBox.Show(response.StatusCode.ToString());
         }
-        */
 
         private void btnPost_Click(object sender, EventArgs e)
         {
-            var options = new RestClientOptions("http://localhost:8081");
+            var codigo = txtCodigo.Text;
+
+            var options = new RestClientOptions("http://localhost:8090");
             var client = new RestClient(options);
-            var request = new RestRequest("/students", Method.Post);
-
-            // Obtener los datos del estudiante desde los campos de texto
-            var estudiante = new Estudiante
-            {
-                codigo = int.Parse(txtCodigo.Text),
-                nombre = txtNombre.Text
-            };
-
-            request.AddJsonBody(estudiante);
-
-            // Convertir el objeto a JSON
-            //var estudianteJson = JsonSerializer.Serialize(estudiante);
-
-            // Agregar el objeto JSON al cuerpo de la solicitud
-            //request.AddStringBody(estudianteJson, ContentType.Json);
+            var request = new RestRequest("/estudiantes/" + codigo, Method.Post);
 
             var response = client.Execute(request);
-
-            if (response.IsSuccessful)
-            {
-                MessageBox.Show("Estudiante registrado correctamente: " + response.StatusDescription);
-                txtCodigo.Text = "";
-                txtNombre.Text = "";
-            }
-            else
-            {
-                MessageBox.Show("Error al registrar estudiante: " + response.StatusDescription);
-            }
+            MessageBox.Show(response.StatusCode.ToString());
         }
     }
 }
