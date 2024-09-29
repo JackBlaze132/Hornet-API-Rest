da1da78f32a59763878b79f7e9a9cda4d3b61d08
Jaime Rodriguez <jaimedanielmontana@gmail.com>
2024-09-29 16:17:17 -0500

    GUIAdd, Edit, Update, Delete, Search compleatly Created.
    GUIList needs implemtation

---
+++ Client/PClienteEstudiante/Main.cs
@@ -0,0 +1,53 @@
+﻿using PClienteEstudiante.view;
+using PClienteEstudiante.view.motorcycle;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PClienteEstudiante
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void addMotorcycleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            GUIAddMotorcycle gUIAddMotorcycle = new GUIAddMotorcycle();
+            gUIAddMotorcycle.Show();
+        }
+
+        private void searchMotorcycleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            GUISearchMotorcycle gUISearchMotorcycle = new GUISearchMotorcycle();
+            gUISearchMotorcycle.Show();
+        }
+
+        private void deleteMotorcycleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            GUIDeleteMotorcycle gUIDeleteMotorcycle = new GUIDeleteMotorcycle();
+            gUIDeleteMotorcycle.Show();
+        }
+
+        private void updateMotorcycleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            GUIUpdateMotorcycle gUIUpdateMotorcycle = new GUIUpdateMotorcycle();
+            gUIUpdateMotorcycle.Show();
+        }
+
+        private void listMotorcycleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            GUIListMotorcycle gUIListMotorcycle = new GUIListMotorcycle();
+            gUIListMotorcycle.Show();
+        }
+    }
+}
---
+++ Client/PClienteEstudiante/view/motorcycle/GUIAddMotorcycle.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace PClienteEstudiante.view
+{
+    partial class GUIAddMotorcycle
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtIdMoto = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtBrandMoto = new System.Windows.Forms.TextBox();
+            this.txtPriceMoto = new System.Windows.Forms.TextBox();
+            this.txtFroktype = new System.Windows.Forms.TextBox();
+            this.btnAddMoto = new System.Windows.Forms.Button();
+            this.boxABS = new System.Windows.Forms.CheckBox();
+            this.boxHelmet = new System.Windows.Forms.CheckBox();
+            this.datePickerMotorcycle = new System.Windows.Forms.DateTimePicker();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtModelMotorcycle = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(299, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(175, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Add Motorcycle";
+            // 
+            // txtIdMoto
+            // 
+            this.txtIdMoto.Location = new System.Drawing.Point(304, 76);
+            this.txtIdMoto.Name = "txtIdMoto";
+            this.txtIdMoto.Size = new System.Drawing.Size(376, 20);
+            this.txtIdMoto.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(101, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Id Moto:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(101, 124);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Brand:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(101, 159);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 16);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Price:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(101, 200);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(42, 16);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "SNID:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(101, 236);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(119, 16);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Does it have ABS?";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(101, 279);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(69, 16);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "ForkType:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(101, 318);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(156, 16);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "Does it include a helmet?";
+            // 
+            // txtBrandMoto
+            // 
+            this.txtBrandMoto.Location = new System.Drawing.Point(304, 119);
+            this.txtBrandMoto.Name = "txtBrandMoto";
+            this.txtBrandMoto.Size = new System.Drawing.Size(376, 20);
+            this.txtBrandMoto.TabIndex = 9;
+            // 
+            // txtPriceMoto
+            // 
+            this.txtPriceMoto.Location = new System.Drawing.Point(304, 159);
+            this.txtPriceMoto.Name = "txtPriceMoto";
+            this.txtPriceMoto.Size = new System.Drawing.Size(376, 20);
+            this.txtPriceMoto.TabIndex = 10;
+            // 
+            // txtFroktype
+            // 
+            this.txtFroktype.Location = new System.Drawing.Point(304, 274);
+            this.txtFroktype.Name = "txtFroktype";
+            this.txtFroktype.Size = new System.Drawing.Size(376, 20);
+            this.txtFroktype.TabIndex = 11;
+            // 
+            // btnAddMoto
+            // 
+            this.btnAddMoto.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAddMoto.Location = new System.Drawing.Point(292, 405);
+            this.btnAddMoto.Name = "btnAddMoto";
+            this.btnAddMoto.Size = new System.Drawing.Size(154, 53);
+            this.btnAddMoto.TabIndex = 12;
+            this.btnAddMoto.Text = "Add";
+            this.btnAddMoto.UseVisualStyleBackColor = true;
+            // 
+            // boxABS
+            // 
+            this.boxABS.AutoSize = true;
+            this.boxABS.Location = new System.Drawing.Point(304, 234);
+            this.boxABS.Name = "boxABS";
+            this.boxABS.Size = new System.Drawing.Size(15, 14);
+            this.boxABS.TabIndex = 13;
+            this.boxABS.UseVisualStyleBackColor = true;
+            // 
+            // boxHelmet
+            // 
+            this.boxHelmet.AutoSize = true;
+            this.boxHelmet.Location = new System.Drawing.Point(304, 316);
+            this.boxHelmet.Name = "boxHelmet";
+            this.boxHelmet.Size = new System.Drawing.Size(15, 14);
+            this.boxHelmet.TabIndex = 14;
+            this.boxHelmet.UseVisualStyleBackColor = true;
+            // 
+            // datePickerMotorcycle
+            // 
+            this.datePickerMotorcycle.Location = new System.Drawing.Point(304, 355);
+            this.datePickerMotorcycle.Name = "datePickerMotorcycle";
+            this.datePickerMotorcycle.Size = new System.Drawing.Size(200, 20);
+            this.datePickerMotorcycle.TabIndex = 15;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(101, 359);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(77, 16);
+            this.label9.TabIndex = 16;
+            this.label9.Text = "Arrival Date";
+            // 
+            // txtModelMotorcycle
+            // 
+            this.txtModelMotorcycle.Location = new System.Drawing.Point(304, 195);
+            this.txtModelMotorcycle.Name = "txtModelMotorcycle";
+            this.txtModelMotorcycle.Size = new System.Drawing.Size(376, 20);
+            this.txtModelMotorcycle.TabIndex = 17;
+            // 
+            // GUIAddMotorcycle
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(765, 470);
+            this.Controls.Add(this.txtModelMotorcycle);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.datePickerMotorcycle);
+            this.Controls.Add(this.boxHelmet);
+            this.Controls.Add(this.boxABS);
+            this.Controls.Add(this.btnAddMoto);
+            this.Controls.Add(this.txtFroktype);
+            this.Controls.Add(this.txtPriceMoto);
+            this.Controls.Add(this.txtBrandMoto);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtIdMoto);
+            this.Controls.Add(this.label1);
+            this.Name = "GUIAddMotorcycle";
+            this.Text = "Add Motorcycle";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtIdMoto;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtBrandMoto;
+        private System.Windows.Forms.TextBox txtPriceMoto;
+        private System.Windows.Forms.TextBox txtFroktype;
+        private System.Windows.Forms.Button btnAddMoto;
+        private System.Windows.Forms.CheckBox boxABS;
+        private System.Windows.Forms.CheckBox boxHelmet;
+        private System.Windows.Forms.DateTimePicker datePickerMotorcycle;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtModelMotorcycle;
+    }
+}
