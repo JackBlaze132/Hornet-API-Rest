ee5503d1b2e4f7d81dfd0abb4b56273c4db0b8cb
JackBlaze132 <ederdmc@hotmail.com>
2024-10-14 00:42:20 -0500

    Implemented auto-imports to astro project

--- AstroClient/astro.config.mjs
+++ AstroClient/astro.config.mjs
@@ -1,14 +1,33 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-
-
-
+import AutoImport from 'unplugin-auto-import/vite';
 import tailwind from '@astrojs/tailwind';
 
-
-
-// https://astro.build/config
+// Configuración de Astro
 export default defineConfig({
-    integrations: [react(), tailwind()],
+  integrations: [react(), tailwind()],
+  vite: {
+    plugins: [
+      AutoImport({
+        imports: [
+          {
+            '@nextui-org/react': [
+              'Button',  // Agrega los componentes de NextUI que vas a utilizar
+              'ButtonGroup',
+              'Card',
+              'Input',
+              'Modal',
+              'Avatar',
+              'Tooltip',
+            ],
+          },
+        ],
+        dts: true,  // Genera un archivo .d.ts si estás usando TypeScript
+        eslintrc: {
+          enabled: true,  // Genera configuración de ESLint si lo estás usando
+        },
+      }),
+    ],
+  },
 });
