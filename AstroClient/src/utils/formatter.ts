6437370ef991468da68920b7f52c06bad1714f5a
JackBlaze132 <ederdmc@hotmail.com>
2024-11-13 21:35:42 -0500

    implemented mull bodywork behaviour in astro UI

--- AstroClient/src/utils/formatter.ts
+++ AstroClient/src/utils/formatter.ts
@@ -23,8 +23,11 @@ class Formatter {
         const hours = String(date.getHours()).padStart(2, '0');
         const minutes = String(date.getMinutes()).padStart(2, '0');
         return ` ${day}/${month}/${year} - ${hours}:${minutes}`;
-      }
-    
+    }
+
+    public nulltoN_A(value: any): string {
+        return value ? value : 'N/A';
+    }
   }
   
   export default Formatter;
